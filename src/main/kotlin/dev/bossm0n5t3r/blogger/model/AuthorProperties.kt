a65e3cdc2c023fc6079a2aa9c12da45ea0634847
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-03-18 00:16:20 +0900

    [2.5.10] fix: Records and @ConfigurationProperties
    - @JvmRecord 로 변경

--- src/main/kotlin/dev/bossm0n5t3r/blogger/model/AuthorProperties.kt
+++ src/main/kotlin/dev/bossm0n5t3r/blogger/model/AuthorProperties.kt
@@ -3,6 +3,7 @@ package dev.bossm0n5t3r.blogger.model
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 
+@JvmRecord
 @ConfigurationProperties("blogger.author")
 @ConstructorBinding
 data class AuthorProperties(
