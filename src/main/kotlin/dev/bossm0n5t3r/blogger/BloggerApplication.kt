b4c7e05f331e193c4f32c9fe846bba02ac6f4913
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-03-17 23:47:20 +0900

    [2.5.10] Application Startup

--- src/main/kotlin/dev/bossm0n5t3r/blogger/BloggerApplication.kt
+++ src/main/kotlin/dev/bossm0n5t3r/blogger/BloggerApplication.kt
@@ -1,11 +1,17 @@
 package dev.bossm0n5t3r.blogger
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class BloggerApplication
 
 fun main(args: Array<String>) {
-    runApplication<BloggerApplication>(*args)
+//    runApplication<BloggerApplication>(*args)
+    val app = SpringApplication(BloggerApplication::class.java).apply {
+        this.applicationStartup = BufferingApplicationStartup(10000)
+    }
+    app.run(*args)
 }
