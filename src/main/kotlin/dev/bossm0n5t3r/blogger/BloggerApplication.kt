62a4d9d3cc69dc4b49cb7c78a6888b410d3aa703
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-03-18 00:11:40 +0900

    [2.5.10] Records and @ConfigurationProperties

--- src/main/kotlin/dev/bossm0n5t3r/blogger/BloggerApplication.kt
+++ src/main/kotlin/dev/bossm0n5t3r/blogger/BloggerApplication.kt
@@ -1,11 +1,14 @@
 package dev.bossm0n5t3r.blogger
 
+import dev.bossm0n5t3r.blogger.model.AuthorProperties
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(AuthorProperties::class)
 class BloggerApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/dev/bossm0n5t3r/blogger/model/AuthorProperties.kt
@@ -0,0 +1,12 @@
+package dev.bossm0n5t3r.blogger.model
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConfigurationProperties("blogger.author")
+@ConstructorBinding
+data class AuthorProperties(
+    val firstName: String,
+    val lastName: String,
+    val email: String
+)
